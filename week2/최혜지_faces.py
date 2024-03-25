8b8abc10bb2b145c0753b6ef2154a288e1a035c4
HyeJi519 <163080351+HyeJi519@users.noreply.github.com>
2024-03-25 22:46:10 +0900

    Add files via upload

---
+++ week2/최혜지_faces.py
@@ -0,0 +1,14 @@
+def main() :
+    emoji = convert_emoji(input("You can use ':)' or ':(' to express your feelings in)"))
+    print (emoji)
+
+def convert_emoji(d) :
+    if d == ":)" :
+        return "🙂"
+
+    elif d == ":(" :
+        return "🙁"
+    else :
+        print("You can use only ':)' or ':('")
+
+main()
---
+++ week2/최혜지_퀴즈_2주차.py
@@ -0,0 +1,4 @@
+from random import *
+
+study_date = randint(4, 29)
+print(f'오프라인 스터디 모임 날짜는 매월 {study_date} 일로 선정되었습니다.')
