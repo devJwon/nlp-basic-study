08875316479659982c2cec11b716e13c24c27c6f
k.chaean <133614461+kchaean@users.noreply.github.com>
2024-04-02 08:14:21 +0900

    강채안_extensions.py

--- week3/강채안_extensions.py
+++ week3/강채안_extensions.py
@@ -14,7 +14,7 @@ if extension in extension_list:
     file_suffix = '.image/' + extension
     print("file's media type: " + name + file_suffix)
     
-#     각각 확장자 결정해야 하는데.. 일단 하나로 통일했습니다..ㅎ
+#     각각 미디어타입 결정해야 하는데.. 일단 하나로 통일했습니다..ㅎ(게으름 이슈)
 #     print('.image/' + extenstion)
 
 else:
