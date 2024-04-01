2be1590d9cd45d964726993ecf9a39ee2e4a93c4
k.chaean <133614461+kchaean@users.noreply.github.com>
2024-04-02 08:13:14 +0900

    Add files via upload

---
+++ week3/강채안_extensions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+file_name = input("file name (check your file's extenstion): ")
+name = file_name.lower().split('.')[0]
+extension = file_name.lower().split('.')[-1]
+
+extension_list = ['gif', 'jpg', 'jpeg', 'png', 'pdf', 'txt', 'zip']
+
+if extension in extension_list:
+    file_suffix = '.image/' + extension
+    print("file's media type: " + name + file_suffix)
+    
+#     각각 확장자 결정해야 하는데.. 일단 하나로 통일했습니다..ㅎ
+#     print('.image/' + extenstion)
+
+else:
+    print('application/octet-stream')
+
+
+# In[ ]:
+
+
+
+
