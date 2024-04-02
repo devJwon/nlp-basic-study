74d3b585a095d874f7422775c750e9de18ce7157
beammeup1229 <57001835+beammeup1229@users.noreply.github.com>
2024-04-02 11:52:06 +0900

    Update 박서윤_format_name.py

--- week3/박서윤_format_name.py
+++ week3/박서윤_format_name.py
@@ -1,3 +1,4 @@
+# title 사용
 while True:
   temp_name = input("What is your name sir/ma'am? ")
   
@@ -8,3 +9,18 @@ while True:
     
   else:
     print("Error: only first and last name please")
+
+
+# caplitalize 사용
+while True:
+    temp_name = input("What is your name sir/ma'am? ")
+
+    if len(temp_name.split()) == 2:
+        real_name = temp_name.split()
+        first = real_name[0].caplitalize()
+        last = real_name[1].caplitalize()
+        new_name = first+" "+last
+
+        print(f"Welcome Aboard, {new_name}!")
+    else:
+        print("Error: only first and last name please")
