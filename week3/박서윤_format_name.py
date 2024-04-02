9c1f54ad234b40da828a30d8304b3fa8346e92aa
beammeup1229 <57001835+beammeup1229@users.noreply.github.com>
2024-04-02 11:47:35 +0900

    Update 박서윤_format_name.py

--- week3/박서윤_format_name.py
+++ week3/박서윤_format_name.py
@@ -1,6 +1,8 @@
-temp_name = input("What is your name sir/ma'am? ")
-if len(temp_name) ==2:
-  real_name = temp_name.title()
-  print(f"Welcome, {real_name}!")
-else:
-  print("only first and last name please")
+while True:
+  temp_name = input("What is your name sir/ma'am? ")
+  if len(temp_name.split()) ==2:
+    real_name = temp_name.title()
+    print(f"Welcome, {real_name}!")
+    break
+  else:
+    print("only first and last name please")
