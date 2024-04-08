cab7e39cc96da33510fc0dc2dc204201bee4d711
HyeJi519 <163080351+HyeJi519@users.noreply.github.com>
2024-04-09 07:56:59 +0900

    Add files via upload

---
+++ week4/최혜지_interpreter.py
@@ -0,0 +1,21 @@
+def main() :
+    expression = input("Enter an arithmetic expression 'x y z' (x and z is number, y is mathematical symbols) : ")
+    x_str, operator, z_str = expression.split()
+    
+    x = int(x_str)
+    z = int(z_str)
+
+    if operator == "+" :
+        result = x + z
+    elif operator == "-" :
+        result = x - z
+    elif operator == "*" :
+        result = x * z
+    elif operator == "/" :
+        result = x / z
+    else :
+        raise ValueError("Unsupported operator")
+    
+    print(f"{result: .1f}")
+
+main()
