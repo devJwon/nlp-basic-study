e2be34fbb5d5b02930ffb62162fe55d36f965caa
k.chaean <133614461+kchaean@users.noreply.github.com>
2024-04-09 10:44:51 +0900

    Update 강채안_quiz4.py

--- week4/강채안_quiz4.py
+++ week4/강채안_quiz4.py
@@ -12,15 +12,7 @@ for i in range(1, 21):
     list.append(i)
     shuffle(list)
 
-# print("치킨 당첨자: ", sample(list, 1))
-# print("커피 당첨자: ", sample(list, 3))
-
 luckys = sample(list, 4)
 
 print('치킨 당첨자: ', luckys[0])
 print('커피 당첨자: ', luckys[1:])
-
-
-# print('치킨 당첨자: {0}'.format(luckys[0]))
-# print('커피 당첨자: {0}'.format(luckys[1:]))
-
