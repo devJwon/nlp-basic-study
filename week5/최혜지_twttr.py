ec11e7ce77cc168a558cd792226a48bbd77ac537
HyeJi519 <163080351+HyeJi519@users.noreply.github.com>
2024-04-30 11:44:01 +0900

    Add files via upload

---
+++ week5/최혜지_twttr.py
@@ -0,0 +1,24 @@
+"""
+When texting or tweeting, it’s not uncommon to shorten words to save time or space, 
+as by omitting vowels, much like Twitter was originally called twttr. 
+In a file called twttr.py, 
+implement a program that prompts the user for a str of text and then outputs that same text but with all vowels (A, E, I, O, and U) omitted, 
+whether inputted in uppercase or lowercase.
+"""
+def main() :
+    user_input = del_aeiou(str(input("Enter any word : ")))
+    print(user_input)
+
+element = ['a', 'e', 'i', 'o', 'u']
+
+def del_aeiou(word) :
+    new_word = []
+    for char in word :
+        if char not in element :
+            new_word.append(char)
+    return ''.join(new_word)
+
+            
+main()
+
+        
